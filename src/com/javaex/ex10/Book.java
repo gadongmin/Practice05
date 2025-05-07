dc3731a9f26e7b32c829c49be7e6b8e23d95fe06
gadongmin <dmkim777@naver.com>
2025-05-07 17:19:49 +0900

    20250507(1)

--- src/com/javaex/ex10/Book.java
+++ src/com/javaex/ex10/Book.java
@@ -1,9 +1,69 @@
 package com.javaex.ex10;
 
 public class Book {
-    
-    
-    
-    
-    
+	// 필드
+	private int bookNo;
+	private String title;
+	private String author;
+	private int stateCode = 1;
+
+	// 생성자
+	public Book() {
+	}
+
+	public Book(int bookNo, String title, String author) {
+		this.bookNo = bookNo;
+		this.title = title;
+		this.author = author;
+	}
+
+	// 메소드-gs
+	public int getBookNo() {
+		return bookNo;
+	}
+
+	public void setBookNo(int book) {
+		this.bookNo = bookNo;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getStateCode() {
+		return stateCode;
+	}
+
+	public void setStateCode(int stateCode) {
+		this.stateCode = stateCode;
+	}
+
+	// 메소드-일반
+	public void rent() {
+		this.stateCode = 0;
+		System.out.println(title + "이(가) 대여 됐습니다.");
+	}
+
+	public void print() {
+		String state;
+		if (this.stateCode == 1) {
+			state = "재고있음";
+		} else {
+			state = "대여중";
+		}
+		System.out.println(bookNo + "  제목: " + title + ", 작가: " + author + ", 대여 유무: " + state);
+	}
+
 }
